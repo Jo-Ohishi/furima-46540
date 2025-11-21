11a6833c8068808c1767cd13922ba410b5658350
Jo-Ohishi <ooishi_catdog@icloud.com>
2025-11-21 19:34:18 +0900

    販売手数料

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,5 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
-import "@hotwired/turbo-rails"
+import "channels"
 import "controllers"
+// 💡 作成した price.js を読み込む
+import "./price"
--- app/models/item.rb
+++ app/models/item.rb
@@ -3,5 +3,26 @@ class Item < ApplicationRecord
   belongs_to_active_hash :condition
   belongs_to_active_hash :shipping_fee_payer
   belongs_to_active_hash :prefecture
-  belongs_to_active_hash :shipping_day
+  belongs_to_active_hash :shipping_day_id
+
+  belongs_to :user
+
+  validates :price, presence: true
+
+  validates :price, numericality: {
+    only_integer: true,
+    greater_than_or_equal_to: 300,
+    less_than_or_equal_to: 9_999_999,
+    message: 'は、¥300〜¥9,999,999の範囲内の半角数値で入力してください'
+  }
+
+  validates :name, presence: true
+  validates :info, presence: true
+  validates :image, presence: true
+
+  validates :category_id, numericality: { other_than: 1, message: 'を選択してください' }
+  validates :condition_id, numericality: { other_than: 1, message: 'を選択してください' }
+  validates :shipping_fee_payer_id, numericality: { other_than: 1, message: 'を選択してください' }
+  validates :prefecture_id, numericality: { other_than: 1, message: 'を選択してください' }
+  validates :shipping_day_id, numericality: { other_than: 1, message: 'を選択してください' }
 end
