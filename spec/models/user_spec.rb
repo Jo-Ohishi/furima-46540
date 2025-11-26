5fffc9db5dd32e3083b677828ebd62624866847b
Jo-Ohishi <ooishi_catdog@icloud.com>
2025-11-26 18:09:01 +0900

    単体テストコード＿修正

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -99,6 +99,31 @@ RSpec.describe User, type: :model do
         user.valid?
         expect(user.errors.full_messages).to include('Password は英字と数字を両方含む必要があります')
       end
+
+      it '名（全角）が空だと登録できない' do
+        user.first_name = ''
+        user.valid?
+        expect(user.errors.full_messages).to include('First name can\'t be blank')
+      end
+
+      it '名（カナ）が空だと登録できない' do
+        user.first_name_kana = ''
+        user.valid?
+        expect(user.errors.full_messages).to include('First name kana can\'t be blank')
+      end
+
+      it '姓（カナ）が空だと登録できない' do
+        user.last_name_kana = ''
+        user.valid?
+        expect(user.errors.full_messages).to include('Last name kana can\'t be blank')
+      end
+
+      it '全角文字を含むパスワードでは登録できない' do
+        user.password = 'ｐａｓｓｗｏｒｄ'
+        user.password_confirmation = 'ｐａｓｓｗｏｒｄ'
+        user.valid?
+        expect(user.errors.full_messages).to include('Password は英字と数字を両方含む必要があります')
+      end
     end
   end
 end
