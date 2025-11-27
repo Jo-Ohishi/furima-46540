2884316f1b362dcff8200ac0dae2afc24ea62ad6
Jo-Ohishi <ooishi_catdog@icloud.com>
2025-11-27 21:11:03 +0900

    実装

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,6 @@ Rails.application.routes.draw do
   get 'items/index'
   root to: "items#index" 
   get 'users/show'
-  # resources :categories do
-  #   resources :items, only: [:index, :new, :create]
-  # end
   resources :items, only: [:index, :create, :new, :show, :edit, :update, :destroy]
   resources :users, only: [:show]
 end
