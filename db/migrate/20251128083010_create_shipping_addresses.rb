023d6ff079e4d3ae92e70698d7d4fa294faec513
Jo-Ohishi <ooishi_catdog@icloud.com>
2025-11-28 17:39:20 +0900

    db

---
+++ db/migrate/20251128083010_create_shipping_addresses.rb
@@ -0,0 +1,14 @@
+class CreateAddresses < ActiveRecord::Migration[7.1]
+  def change
+    create_table :addresses do |t|
+      t.references :order, null: false, foreign_key: true
+      t.string :postal_code
+      t.integer :prefecture_id
+      t.string :city
+      t.string :street_addresses
+      t.string :building_name
+      t.string :phone_number
+      t.timestamps
+    end
+  end
+end
