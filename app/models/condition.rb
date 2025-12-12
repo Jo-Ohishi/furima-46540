72376d1d7f93a909519d1faa8460acee0158e61f
Jo-Ohishi <ooishi_catdog@icloud.com>
2025-12-12 15:06:28 +0900

    修正

--- app/models/condition.rb
+++ app/models/condition.rb
@@ -1,6 +1,6 @@
 class Condition < ActiveHash::Base
   self.data = [
-    { id: 2, name: '新品、未使用' },
+    { id: 2, name: '新品・未使用' },
     { id: 3, name: '未使用に近い' },
     { id: 4, name: '目立った傷や汚れなし' },
     { id: 5, name: 'やや傷や汚れあり' },
